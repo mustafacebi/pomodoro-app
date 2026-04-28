0042e764242d62fabcebe3d642369bf413592ad0
Mustafa Emin <152057917+mustafacebi@users.noreply.github.com>
2026-04-28 11:28:09 +0300

    commit ilk

--- App.js
+++ App.js
@@ -75,8 +75,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
     alignItems: 'center',
   },
